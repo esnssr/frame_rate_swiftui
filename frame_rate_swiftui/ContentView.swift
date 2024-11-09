b45bc128bc8f7390bb388a1cf3aaf0a0b4448f4b
Eslam Nasser <eslamoz.nasser@gmail.com>
2024-11-09 18:46:05 +0100

    Initial Commit

---
+++ frame_rate_swiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  frame_rate_swiftui
+//
+//  Created by Eslam Nasser on 2024-11-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ frame_rate_swiftui/frame_rate_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  frame_rate_swiftuiApp.swift
+//  frame_rate_swiftui
+//
+//  Created by Eslam Nasser on 2024-11-09.
+//
+
+import SwiftUI
+
+@main
+struct frame_rate_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
